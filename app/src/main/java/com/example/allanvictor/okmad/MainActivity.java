45bb21f22cd687f31afb1f29a810a21a3da8cb89
allan.victor <doislan@gmail.com>
2018-03-23 14:07:19 -0300

    Adds text to speech support

--- app/src/main/java/com/example/allanvictor/okmad/MainActivity.java
+++ app/src/main/java/com/example/allanvictor/okmad/MainActivity.java
@@ -1,27 +1,52 @@
 package com.example.allanvictor.okmad;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech engine;
+    private Excuse excuse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        engine = new TextToSpeech(this,this);
+        excuse = new Excuse(getResources().openRawResource(R.raw.excuses_list));
         setContentView(R.layout.activity_main);
-        updateExcuseText();
+        updateExcuse();
     }
 
-    private void updateExcuseText() {
-        TextView excuse_text = findViewById(R.id.excuse_text);
-        Excuse excuse = new Excuse(getResources().openRawResource(R.raw.excuses_list));
-        excuse_text.setText(excuse.getRandom());
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            engine.setLanguage(Locale.forLanguageTag("pt-BR"));
+        }
     }
 
     public void updateExcuseView(View view) {
-        updateExcuseText();
+        updateExcuse();
+    }
+
+    private void updateExcuse() {
+        String current_excuse = excuse.getRandom();
+
+        updateExcuseText(current_excuse);
+        speakExcuse(current_excuse);
+    }
+
+    private void updateExcuseText(String message) {
+        TextView excuse_text = findViewById(R.id.excuse_text);
+        excuse_text.setText(message);
+    }
+
+    private void speakExcuse(String message) {
+        engine.speak(message, TextToSpeech.QUEUE_FLUSH, null, null);
     }
 
 }
