d7bc2f70adbb1b29f66d9d84185fcdfac9cdfeaf
allan.victor <doislan@gmail.com>
2018-03-23 13:51:25 -0300

    Updates main activity to show excuses

--- app/src/main/java/com/example/allanvictor/okmad/MainActivity.java
+++ app/src/main/java/com/example/allanvictor/okmad/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.allanvictor.okmad;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateExcuseText();
     }
+
+    private void updateExcuseText() {
+        TextView excuse_text = findViewById(R.id.excuse_text);
+        Excuse excuse = new Excuse(getResources().openRawResource(R.raw.excuses_list));
+        excuse_text.setText(excuse.getRandom());
+    }
+
+    public void updateExcuseView(View view) {
+        updateExcuseText();
+    }
+
 }
